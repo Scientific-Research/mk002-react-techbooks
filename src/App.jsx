55609bc73e51c070911534d02660e016811c1b57
Maximilian <89244567+Scientific-Research@users.noreply.github.com>
2022-11-03 23:30:17 +0100

    Add files via upload
    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.scss";
+import techbooks from "./data/techbooks.json";
+import { Techbook } from "./components/Techbook";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>{techbooks.length} Tech Books</h1>
+      <div className="books">
+        {techbooks.map((techbook, index) => {
+          return <Techbook key={index} techbook={techbook} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
